fb9af3ceb8087b688a8c773d34cfcb8410ec826f
Asanka <asankalk@gmail.com>
2022-12-30 17:09:16 +0800

    lec 8

--- js/script.js
+++ js/script.js
@@ -2,5 +2,10 @@ $(function() {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-  // $(".red-box").fadeOut(2000);
+  $(".red-box").fadeTo(3000,0.2);
+  $(".green-box").fadeTo(4000,0.5);
+  $(".blue-box").fadeTo(5000,0.2);
+
+  $(".blue-box").fadeToggle();
+  $(".blue-box").fadeToggle();
 });
