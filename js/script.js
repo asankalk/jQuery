c2daf7781082e97c8633ce424690049ba2d5b643
Asanka <asankalk@gmail.com>
2023-01-05 06:00:36 +0800

    DOM

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//CSS Selectors
 $(function() {
     //$("p").css("background-color", "rgba(180,180,30, 0.8");
     //$(".red-box").css("background-color", "rgba(180,180,30, 0.8");
@@ -5,5 +6,16 @@ $(function() {
     // $("input[type='email']").css("background-color", "rgba(180,180,30, 0.8");
     // $("h2, p, input").css("background-color", "rgba(180,180,30, 0.8");
     // $("p:first").css("background-color", "rgba(180,180,30, 0.8");
-    $("li:even").css("background-color", "rgba(180,180,30, 0.8");
+    // $("li:even").css("background-color", "rgba(180,180,30, 0.8");
+});
+
+// Filtering
+// $(function(){
+//     $("#list").find("li").filter(":even").css("background-color", "rgba(180,180,30, 0.8");
+// });
+
+// DOM
+
+$(function(){
+
 });
